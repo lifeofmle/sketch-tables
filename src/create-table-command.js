109fb3fa6259264f7922cf4fbf64310725471c54
mle <michaell@kounta.com>
2018-04-01 23:38:39 +1000

    Initial commit

---
+++ src/create-table-command.js
@@ -0,0 +1,14 @@
+export default function(context) {
+  // TODO: show modal to get inputs
+  const rows = 4;
+  const columns = 3;  
+
+  // Check for symbol existance
+  // If no create them
+
+  const masterHeader = "";
+  const masterCell = "";
+  const masterCellEnd = "";
+
+  // Create
+}
---
+++ src/insert-row-command.js
@@ -0,0 +1,3 @@
+export default function(context) {
+    context.document.showMessage("🛠 Coming soon");
+}
